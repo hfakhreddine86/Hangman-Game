a4e9fa0b70265d5e1e67840dfbd9e9c229019cd6
hfakhreddine86 <hfakhreddine86@gmail.com>
2017-10-27 10:03:50 -0700

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,56 @@
-var words = ["RAGNAR", "SPAGHETTI", "MAGIC", "JOHNSON", "CRACKER", "HAMBURGER", "HALLOWEEN"];
+//Global Variables
+
+//Arrays and variables for holding data
+var words = ["ragnar", "spaghetti", "magic", "johnson", "cracker", "hamburger", "halloween"];
 var wordSelector = "";
 var totalLetters = [];
 var underscore = 0;
+var underscoreCorrect = [];
+var lettersGuessed = [];
+
+//Counters
 var winsCounter = 0;
-var guessesRemaining = 9;
+var livesRemaining = 9;
+
+//Functions (resuable blocks of code that i will call upon when needed)
+//=====================================================================
+
+//start game when any key is pressed
 
 
-function startGame() {
+function startGame () {
     wordSelector = words[Math.floor(Math.random() * words.length)];
     totalLetters = wordSelector.split("");
     underscore = totalLetters.length;
 
+    //reset
 
-    var guessesRemaining = 9;
-    var lettersGuessed = [];
+    livesRemaining = 9;
+    lettersGuessed = [];
+    underscoreCorrect = [];
+
+    // get underscore characters
+    for (var i = 0; i < underscore; i++) {
+        underscoreCorrect.push("_");
+    }
+
+    // HTML
+    document.getElementById("currentWord").innerHTML = underscoreCorrect.join(" ");
+    document.getElementById("remainingGuesses").innerHTML = livesRemaining;
+    document.getElementById("wins").innerHTML = winsCounter;
 
 
+    //Testing
     console.log(wordSelector);
     console.log(totalLetters);
     console.log(underscore);
+    console.log(underscoreCorrect);
 }
 
-function letterCheck(letter) {
+//Main Process
+//===========================
 
-    var letterInWord = false;
-
-    for (var i = 0; i < underscore; i++) {
-        if (wordSelector[i] === letter) {
-            letterInWord = true;
-        }
-    else {
-        lettersGuessed.push(letter);
-        guessesRemaining--;
-    }
-
-}
-
-function roundOver() {
-    console.log("Win Count: " + winsCounter + " | Guesses Left: " + guessesRemaining);
-
-    document.getElementById("remainingGuesses").innerHTML = guessesRemaining;
-    document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
-
-}
-
+//starts game
 startGame();
 
-document.onkeyup = function (event) {
-
-    var letterGuessed = String.fromCharCode(event.keyCode).toUpperCase();
-    letterCheck(letterGuessed);
-
-    roundOver();
-
-    console.log(letterGuessed);
-}
+//associates key clicks
