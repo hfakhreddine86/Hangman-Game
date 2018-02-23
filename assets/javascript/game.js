4078c1d79b5f147ae92d4a39640250e3a728bfb6
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-02-22 21:43:34 -0800

    working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,80 @@
-//Global Variables
-
-//Arrays and variables for holding data
-var words = ["ragnar", "spaghetti", "magic", "johnson", "cracker", "hamburger", "halloween"];
+var words = ["RAGNAR", "SPAGHETTI", "MAGIC", "JOHNSON", "CRACKER", "HAMBURGER", "HALLOWEEN"];
 var wordSelector = "";
 var totalLetters = [];
 var underscore = 0;
 var underscoreCorrect = [];
 var lettersGuessed = [];
-
-//Counters
 var winsCounter = 0;
-var livesRemaining = 9;
-
-//Functions (resuable blocks of code that i will call upon when needed)
-//=====================================================================
-
-//start game when any key is pressed
+var guessesRemaining = 9;
 
 
-function startGame () {
+function startGame() {
     wordSelector = words[Math.floor(Math.random() * words.length)];
     totalLetters = wordSelector.split("");
     underscore = totalLetters.length;
 
-    //reset
 
-    livesRemaining = 9;
+    guessesRemaining = 9;
     lettersGuessed = [];
     underscoreCorrect = [];
 
-    // get underscore characters
     for (var i = 0; i < underscore; i++) {
         underscoreCorrect.push("_");
     }
 
-    // HTML
     document.getElementById("currentWord").innerHTML = underscoreCorrect.join(" ");
-    document.getElementById("remainingGuesses").innerHTML = livesRemaining;
+    document.getElementById("remainingGuesses").innerHTML = guessesRemaining;
     document.getElementById("wins").innerHTML = winsCounter;
 
-
-    //Testing
-    console.log(wordSelector);
-    console.log(totalLetters);
-    console.log(underscore);
-    console.log(underscoreCorrect);
 }
 
-//Main Process
-//===========================
+function letterCheck(letter) {
 
-//starts game
+    var letterInWord = false;
+
+    for (var i = 0; i < underscore; i++) {
+        if (wordSelector[i] === letter) {
+            letterInWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var i = 0; i < underscore; i++) {
+            if (wordSelector[i] === letter) {
+                underscoreCorrect[i] = letter;
+            }
+        }
+    } else {
+        lettersGuessed.push(letter);
+        guessesRemaining--;
+    }
+}
+
+function roundOver() {
+    document.getElementById("remainingGuesses").innerHTML = guessesRemaining;
+    
+    document.getElementById("currentWord").innerHTML = underscoreCorrect.join(" ");
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(" ");
+
+    if (totalLetters.toString() === underscoreCorrect.toString()) {
+        winsCounter++;
+        alert("You Won!");
+
+        document.getElementById("wins").innerHTML = winsCounter;
+
+        startGame();
+    } else if (guessesRemaining === 0) {
+        alert("You Lost!");
+
+        startGame();
+    }
+}
+
 startGame();
 
-//associates key clicks
+document.onkeyup = function (event) {
+
+    var letterGuessed = String.fromCharCode(event.keyCode).toUpperCase();
+    letterCheck(letterGuessed);
+
+    roundOver();
+}
---
+++ pseudocode.md
@@ -0,0 +1,37 @@
+# Hangman Game
+
+
+Initialize
+-pick a word
+-create a placeholder "_ _ _ _" e.g.
+-set guesses left to 7
+
+
+Start
+-wait for user input
+    -check if they already guessed the letter
+    -check if the letter is in the word
+        -update placeholder e.g. "w _ _ _"
+        -check if game is over (win)
+    -check if its a loss
+
+-check if game is over
+    -if all letters match > win
+    -if gueeses 0 > loss
+    -if anthing else > game still running
+
+End
+-Game is over
+    -pring out a win screen
+    -give them a button to restart
+
+Any nouns are variables
+    -word
+    -words
+    -placeholder
+    -guesses (#)
+    -wins (#)
+    -losses (#)
+
+
+
